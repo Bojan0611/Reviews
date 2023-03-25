03c0e0bf29001a78e96aed23fc9f6860344e677f
Bojan95 <jbojanowski11@gmail.com>
2023-03-25 15:10:02 +0100

    add form class

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
+from .forms import ReviewForm
+
 # Create your views here.
 
 def review(request):
     if request.method=="POST":
-        entered_username = request.POST['username']
-        print(entered_username)
-        if entered_username == "":
-            return render(request, "reviews/review.html", {
-                "has_error": True
-            })
-        return HttpResponseRedirect("/thank-you")
-    return render(request, "reviews/review.html")
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect("/thank-you")
+    form = ReviewForm()
+    
+    return render(request, "reviews/review.html", {
+        "form": form,
+    })
 
 def thank_you(request):
-    return render(request, "reviews/thank_you.html", {
-        "has_error": False
-    })
+    return render(request, "reviews/thank_you.html")
