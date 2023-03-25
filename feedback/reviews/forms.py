74afc57cbd80988babe61def718bd53c05799497
Bojan95 <jbojanowski11@gmail.com>
2023-03-25 19:58:31 +0100

    forms update, html changes

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,5 +1,10 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField(max_length=100)
+    user_name = forms.CharField(max_length=30,
+                                label="Your name",
+                                error_messages={
+                                    "required": "Your name must not be empty!",
+                                    "max_length": "Please enter shorter name!",
+                                })
     
