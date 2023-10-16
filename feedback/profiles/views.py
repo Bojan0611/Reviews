4618a5363c91d86a493f2c2d6e654ed9882e43aa
Bojan95 <jbojanowski11@gmail.com>
2023-10-17 00:02:42 +0200

    serving media files

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 
 from .models import UserProfile
 
@@ -12,3 +13,9 @@ class CreateProfileView(CreateView):
     model = UserProfile
     fields = "__all__"
     success_url = "/profiles"
+
+class UserProfilesView(ListView):
+    template_name = "profiles/user_profiles.html"
+    model = UserProfile
+    context_object_name = "profiles"
+    
